b4a5e86293a867f2185c8a4a68f13c4f5948a890
Ana-Ruiz1 <apruizmanzur@mail.austral.edu.ar>
2026-04-01 21:02:14 -0300

    Update exercise_compare.py

--- exercise_compare.py
+++ exercise_compare.py
@@ -16,3 +16,11 @@ def compare():
         7 es igual a 7
     """
     pass
+    num1 = int(input("Ingrese un numero: "))
+    num2 = int(input("Ingrese un numero: "))
+    if num1 > num2:
+        print(f"{num1} es mayor que {num2}")
+    elif num1 < num2:
+        print(f"{num2} es mayor que {num1}")
+    else:
+        print(f"{num1} es igual a {num2}")
