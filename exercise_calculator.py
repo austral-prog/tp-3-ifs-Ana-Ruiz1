2ed9e50f2920c50d89b13b34e843f56c94289760
Ana-Ruiz1 <apruizmanzur@mail.austral.edu.ar>
2026-04-14 10:21:46 -0300

    Update exercise_calculator.py

--- exercise_calculator.py
+++ exercise_calculator.py
@@ -22,9 +22,9 @@ def calculator():
         Para las entradas "10", "5" y "x", la salida esperada es:
         Operacion invalida
     """
-    num1 = float(input("Ingrese un numero: "))
-    num2 = float(input("Ingrese un numero: "))
-    operacion = input("Ingrese operacion (+, -, *, /): ")
+    num1 = float(input())
+    num2 = float(input())
+    operacion = input()
     
     if operacion == "+":
         print(f"Resultado: {num1 + num2}")
